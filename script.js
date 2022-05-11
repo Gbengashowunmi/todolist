4135b1a5866b416dd094af51c65ea6ff329ddb99
Gbengashowunmi <gbengashowunmi01@gmail.com>
2022-05-11 23:31:10 +0100

    todo list

---
+++ script.js
@@ -0,0 +1,20 @@
+// select 8 elements
+const alert = document.querySelector('.head');
+const button = document.querySelector('.submit');
+const input = document.querySelector('.input');
+const tasks = document.querySelector('.task');
+const delBtn = document.querySelector('.delete');
+const editBtn = document.querySelector('.edit');
+const clearItems = document.querySelector('.clear-items');
+
+
+
+
+//submit button function
+button.addEventListener('click',()=>{
+    // console.log('love');
+   if (input.value !=='') {
+    console.log('me');
+    
+   }
+})
